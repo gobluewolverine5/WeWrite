454c735891a9cd2853a115cfb8938dbe3b512c15
Evan Hsu <evanjhsu@umich.edu>
2014-01-16 22:33:39 -0500

    Initial Commit

---
+++ WeWrite/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  WeWrite
+//
+//  Created by Evan Hsu on 1/16/14.
+//  Copyright (c) 2014 EECS 441. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ WeWrite/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  WeWrite
+//
+//  Created by Evan Hsu on 1/16/14.
+//  Copyright (c) 2014 EECS 441. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
