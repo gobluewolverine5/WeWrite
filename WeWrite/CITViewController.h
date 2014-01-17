1ca83867be9901ff5737bdf5ab5c739c28a48497
Evan Hsu <evanjhsu@umich.edu>
2014-01-17 12:18:00 -0500

    first commit

---
+++ WeWrite/CITViewController.h
@@ -0,0 +1,13 @@
+//
+//  CITViewController.h
+//  WeWrite
+//
+//  Created by Evan Hsu on 1/16/14.
+//  Copyright (c) 2014 EECS 441. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CITViewController : UIViewController
+
+@end
---
+++ WeWrite/CITViewController.m
@@ -0,0 +1,38 @@
+//
+//  CITViewController.m
+//  WeWrite
+//
+//  Created by Evan Hsu on 1/16/14.
+//  Copyright (c) 2014 EECS 441. All rights reserved.
+//
+
+#import "CITViewController.h"
+
+@interface CITViewController ()
+
+@end
+
+@implementation CITViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
